6884694dbee32dbc1196f362ff576e90b23fb08a
Krishnapriya K M <krishnapriyakm05@example.com>
2026-04-11 23:20:16 +0530

    final commit1

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def play(player):
     return jsonify({"computer": computer, "result": result})  
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)
 
 
 
